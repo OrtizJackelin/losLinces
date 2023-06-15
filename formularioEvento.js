36f09b714d6280c732d9b586c4131fd98df4e94e
OrtizJackelin <134174895+OrtizJackelin@users.noreply.github.com>
2023-06-14 22:02:06 -0300

    Update formularioEvento.js json url

--- formularioEvento.js
+++ formularioEvento.js
@@ -1,7 +1,7 @@
 
 const fetchData = async () => {
    
-    fetch('http://127.0.0.1:5500/data.json')
+    fetch('https://ortizjackelin.github.io/data.json')
     .then ((datos)=>datos.json())
     .then ((datos)=> {
         try {
