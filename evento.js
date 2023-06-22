96c35b4d86de2db4546df944d4ad63cdb527ad5f
claudisjackelin <clau.jack.uni@gmail.com>
2023-06-22 13:45:17 -0300

    Cambios en js de script y

--- evento.js
+++ evento.js
@@ -1,5 +1,4 @@
-const cards = document.querySelector("#card-dinamica");
-const templateCard = document.querySelector("#template-card").content;
+
 var listaEventos;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -54,6 +53,9 @@ const mostrarSpinner = (estado) => {
 };
 
 const pintarDatos = (data) => {
+
+    const cards = document.querySelector("#card-dinamica");
+    const templateCard = document.querySelector("#template-card").content;
 
     const fragment = document.createDocumentFragment();
 
